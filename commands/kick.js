d7cd708f5755cd7d515e6c60b060dcb60b54070a
Joseph Morse <tamed.lionheart@gmail.com>
2019-04-11 08:01:15 -0400

    fix: Kick Needs Permissions
    Added a check for kick permissions of the user initiated kick.

--- commands/kick.js
+++ commands/kick.js
@@ -6,21 +6,23 @@ module.exports = (message) => {
   if (message.content.startsWith('!kick')) {
     const user = message.mentions.users.first()
 
-    if (user) {
-      const member = message.guild.member(user)
+    if (message.member.hasPermission(['KICK_MEMBERS'])) {
+      if (user) {
+        const member = message.guild.member(user)
 
-      if (member) {
-        member.kick()
-          .then(() => message.reply('Successfully kicked ${user.tag'))
-          .catch((err) => {
-            message.reply('I was unable to kick the member')
-            console.error(err)
-          })
+        if (member) {
+          member.kick()
+            .then(() => message.reply(`User ${user.tag} was kicked by ${message.member.displayName}`))
+            .catch((err) => {
+              message.reply('I was unable to kick member')
+              console.error(err)
+            })
+        } else {
+          message.reply('That user isn\'t in this guild')
+        }
       } else {
-        message.reply('That user isn\'n in this guild!')
+        message.reply('You Didn\'t mention the user to kick!')
       }
-    } else {
-      message.reply('You Didn\'t mention the user to kick!')
     }
   }
 }
